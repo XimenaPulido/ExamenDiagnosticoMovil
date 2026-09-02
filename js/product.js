5108a8a937b9647854d8e8d2dc432a9967497ada
XimenaPulidoGutierrez <ximena.pulido.gutierrez@gmail.com>
2026-09-01 18:56:48 -0600

    feature/final-system

---
+++ js/product.js
@@ -0,0 +1,30 @@
+const PRODUCTS_DATA = [
+    {
+        id: 1,
+        title: "Audífonos Inalámbricos",
+        price: 59.99,
+        rating: 5,
+        image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&auto=format&fit=crop&q=60"
+    },
+    {
+        id: 2,
+        title: "Smartwatch Pro",
+        price: 89.99,
+        rating: 5,
+        image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60"
+    },
+    {
+        id: 3,
+        title: "Cámara Digital",
+        price: 199.99,
+        rating: 5,
+        image: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=500&auto=format&fit=crop&q=60"
+    },
+    {
+        id: 4,
+        title: "Mochila Urbana",
+        price: 39.99,
+        rating: 4,
+        image: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=500&auto=format&fit=crop&q=60"
+    }
+];
